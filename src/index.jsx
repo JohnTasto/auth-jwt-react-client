d87699bc2b844df450a1bbe5d164f5f4691c07e1
John Tasto <john@tasto.net>
2017-03-04 00:44:40 -0800

    Refactor routes to separate file

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, browserHistory } from 'react-router'
 
 import createStore from './store'
+import routes from './routes'
 
-import App from './components/app'
-import Signin from './components/auth/signin'
-import Signout from './components/auth/signout'
-import Signup from './components/auth/signup'
-import Feature from './components/feature'
-import RequireAuth from './components/auth/require_auth'
-import Welcome from './components/welcome'
 import { AUTH_USER } from './ducks/auth'
 
 import './style/style.css'
@@ -23,14 +17,6 @@ if (token) store.dispatch({ type: AUTH_USER })
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Welcome} />
-        <Route path="signin" component={Signin} />
-        <Route path="signout" component={Signout} />
-        <Route path="signup" component={Signup} />
-        <Route path="feature" component={RequireAuth(Feature)} />
-      </Route>
-    </Router>
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'))
