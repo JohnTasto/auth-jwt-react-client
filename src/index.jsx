94472f439f5d955c4633407bda2d7368ef937cb1
John Tasto <john@tasto.net>
2017-03-20 21:09:07 -0700

    Remove react-router-redux until bugs are worked out

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+// import { ConnectedRouter as Router } from 'react-router-redux'
 
-import createStore, { history } from './store'
+import createStore/* , { history } */from './store'
 import App from './components/App'
 
 import { AUTH_USER } from './ducks/auth'
@@ -19,8 +20,9 @@ if (token) store.dispatch({ type: AUTH_USER })
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
+    {/* <Router history={history}> */}
+    <Router>
       <App />
-    </ConnectedRouter>
+    </Router>
   </Provider>
   , document.querySelector('.container'))
