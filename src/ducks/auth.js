b7a33fb16353c244b53d78ceb1ad238ba9440a88
John Tasto <john@tasto.net>
2017-04-13 01:56:46 -0700

    Convert action creators to arrow functions

--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -13,74 +13,63 @@ export const SET_AUTH_REDIRECT = 'set_auth_redirect'
 export const FETCH_MESSAGE = 'fetch_message'
 
 
-export function setAuthRedirect(location) {
-  return {
-    type: SET_AUTH_REDIRECT,
-    payload: location,
-  }
-}
+export const setAuthRedirect = location => ({
+  type: SET_AUTH_REDIRECT,
+  payload: location,
+})
 
-export function signUpUser({ email, password }) {
-  return function signUpUserThunk(dispatch) {
-    return axios.post(`${API_ROOT}/signup`, { email, password })
-      .then(response => {
-        localStorage.setItem('token', response.data.token)
-        dispatch({ type: AUTH_USER })
-      })
-      .catch(error => {
-        if (process.env.NODE_ENV === 'development') console.dir(error)  // eslint-disable-line no-console
-        const message = error.response && error.response.status === 422
-          ? error.response.data.error || error.response.data.errorMessage
-          : 'Network error'
-        throw new SubmissionError({ _error: message })
-      })
-  }
-}
+export const signUpUser = ({ email, password }) => dispatch =>
+  axios.post(`${API_ROOT}/signup`, { email, password })
+    .then(response => {
+      localStorage.setItem('token', response.data.token)
+      dispatch({ type: AUTH_USER })
+    })
+    .catch(error => {
+      if (process.env.NODE_ENV === 'development') console.dir(error)  // eslint-disable-line no-console
+      const message = error.response && error.response.status === 422
+        ? error.response.data.error || error.response.data.errorMessage
+        : 'Network error'
+      throw new SubmissionError({ _error: message })
+    })
 
-export function signInUser({ email, password }) {
-  return function signInUserThunk(dispatch) {
-    return axios.post(`${API_ROOT}/signin`, { email, password })
-      .then(response => {
-        localStorage.setItem('token', response.data.token)
-        dispatch({ type: AUTH_USER })
-      })
-      .catch(error => {
-        if (process.env.NODE_ENV === 'development') console.dir(error)  // eslint-disable-line no-console
-        const message = error.response && error.response.status === 401
-          ? 'Invalid credentials'
-          : 'Network error'
-        throw new SubmissionError({ _error: message })
-      })
-  }
-}
+export const signInUser = ({ email, password }) => dispatch =>
+  axios.post(`${API_ROOT}/signin`, { email, password })
+    .then(response => {
+      localStorage.setItem('token', response.data.token)
+      dispatch({ type: AUTH_USER })
+    })
+    .catch(error => {
+      if (process.env.NODE_ENV === 'development') console.dir(error)  // eslint-disable-line no-console
+      const message = error.response && error.response.status === 401
+        ? 'Invalid credentials'
+        : 'Network error'
+      throw new SubmissionError({ _error: message })
+    })
 
-export function signOutUser() {
+export const signOutUser = () => {
   localStorage.removeItem('token')
   return { type: UNAUTH_USER }
 }
 
-export function fetchMessage() {
-  return function fetchMessageThunk(dispatch) {
-    return axios.get(`${API_ROOT}/feature`, {
-      headers: { authorization: `Bearer ${localStorage.getItem('token')}` },
+export const fetchMessage = () => dispatch =>
+  axios.get(`${API_ROOT}/feature`, {
+    headers: { authorization: `Bearer ${localStorage.getItem('token')}` },
+  })
+    .then(response => {
+      dispatch({
+        type: FETCH_MESSAGE,
+        payload: response.data.message,
+      })
     })
-      .then(response => {
-        dispatch({
-          type: FETCH_MESSAGE,
-          payload: response.data.message,
-        })
-      })
-      .catch(error => {
-        if (process.env.NODE_ENV === 'development') console.dir(error)  // eslint-disable-line no-console
-        if (error.response && error.response.status === 401) {
-          dispatch(signOutUser())
-        }
-      })
-  }
-}
+    .catch(error => {
+      if (process.env.NODE_ENV === 'development') console.dir(error)  // eslint-disable-line no-console
+      if (error.response && error.response.status === 401) {
+        dispatch(signOutUser())
+      }
+    })
 
 
-export default function (state = {}, { type, payload }) {
+export default (state = {}, { type, payload }) => {
   switch (type) {  // eslint-disable-line default-case
     case AUTH_USER:
       return { ...state, authenticated: true }
@@ -91,6 +80,5 @@ export default function (state = {}, { type, payload }) {
     case FETCH_MESSAGE:
       return { ...state, message: payload }
   }
-
   return state
 }
